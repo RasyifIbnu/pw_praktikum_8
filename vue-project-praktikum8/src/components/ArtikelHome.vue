7c238b3603ba169bb3e08f4414db9735018f0818
RasyifIbnu <rasyifibnu23@gmail.com>
2024-05-30 17:13:07 +0700

    new file:   vue-project-praktikum8/.eslintrc.cjs

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="https://media.cnn.com/api/v1/images/stellar/prod/comfy-shoes-nike-new-lead.jpg?c=16x9&q=w_800,c_fill" alt="" width="">
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+    </div>
+</template>
